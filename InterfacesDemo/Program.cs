5c2427ef3c9203f7ad4bccacee341d00f29e1f64
kwaterma <kuukusika449@gmail.com>
2025-08-08 17:32:23 +0000

    Added Interfaces example

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InterfacesDemo
+{
+    // Interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Car class
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle class
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
