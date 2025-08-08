32bcf26c52382b1c2d97ed3f16b03af397aa353a
kwaterma <kuukusika449@gmail.com>
2025-08-08 17:26:24 +0000

    Added Abstract Classes and Methods example

---
+++ AbstractClassesDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+
+namespace AbstractClassesDemo
+{
+    // Abstract class
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    // Circle class
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    // Rectangle class
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            Rectangle rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Area of Circle: {circle.GetArea()}");
+            Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
+        }
+    }
+}
